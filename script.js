f5e9a86dc6f60f7d1b92d251249f2ec9a1cba1d1
Nikita Arinchehin <narincehin@gmail.com>
2022-08-11 13:24:18 +0300

    feat: add delete, caps and space buttons

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const button = document.querySelectorAll('.Symb');
+const backspace = document.querySelector('.Backspace')
+const caps = document.querySelector('.Caps')
 const textarea = document.querySelector('textarea');
-
+const space = document.querySelector('.Space');
+const shift = document.querySelector('.Shift')
+//Переменная для CapsLock
+var capsCount = 0;
+//Переменаня для Shift
+var shiftCount = 0;
 
 
 button.forEach(buttonItem => {
@@ -9,3 +16,39 @@ button.forEach(buttonItem => {
     })
 })
 
+//Функция удаления
+backspace.addEventListener("click",()=>{
+    textarea.value = textarea.value.slice(0, -1);
+})
+
+//Функция CapsLock
+caps.addEventListener("click", ()=>{
+    if (capsCount == 0){
+        button.forEach(buttonItem => {
+            buttonItem.innerHTML = buttonItem.innerHTML.toUpperCase();
+            caps.style.backgroundColor = '#444'
+            capsCount = 1;
+        })
+    }
+    else{
+        button.forEach(buttonItem => {
+            buttonItem.innerHTML = buttonItem.innerHTML.toLowerCase();
+            caps.style.backgroundColor = '#222222'
+            capsCount = 0;
+        })
+    }
+})
+
+//Функция space
+space.addEventListener("click", ()=> {
+    textarea.value = textarea.value + " ";
+})
+
+shift.addEventListener("click", () => {
+    if (shiftCount == 0){
+        shift.style.backgroundColor = '#444';
+        button.forEach(buttonItem =>{
+            buttonItem.innerHTML = buttonItem.innerHTML.toUpperCase();
+        })
+    }
+})
